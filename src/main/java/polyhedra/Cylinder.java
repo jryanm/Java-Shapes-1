7edd3b9ddb3336e50c7a1e17b128f66cb3aeec21
Josh Martin <jmart137@odu.edu>
2023-11-11 13:08:24 -0500

    Compiles, all tests pass --> Project done?

--- src/main/java/polyhedra/Cylinder.java
+++ src/main/java/polyhedra/Cylinder.java
@@ -69,6 +69,10 @@ public class Cylinder extends Polyhedron {
     public void setRadius(double r)
     {
         // Implement this function
+        this.radius = r;
+
+        double d = getDiameter();
+        this.boundingBox.setUpperRightVertex(d, d, this.height);
     }
 
     /**
@@ -79,6 +83,10 @@ public class Cylinder extends Polyhedron {
     public void setHeight(double h)
     {
         // Implement this function
+        this.height = h;
+
+        double d = getDiameter();
+        this.boundingBox.setUpperRightVertex(d, d, h);
     }
 
     /**
@@ -94,25 +102,37 @@ public class Cylinder extends Polyhedron {
     @Override
     public Polyhedron clone()
     {
-        return null; // remove this line
-        // Implement this function
+        return new Cylinder(this.radius, this.height);
     }
 
     @Override
     public void read(Scanner scanner)
     {
         // Implement this function
+        this.height = scanner.nextDouble();
+        this.radius = scanner.nextDouble();
+
+        double d = getDiameter();
+        this.boundingBox.setUpperRightVertex(d, d, this.height);
     }
 
     @Override
     public void scale(double scalingFactor)
     {
         // Implement this function
+        this.radius *= scalingFactor;
+        this.height *= scalingFactor;
+        this.boundingBox.scale(scalingFactor);
     }
 
     @Override
     public String toString()
     {
-        return "Cylinder.toString Not Implemented";
+        return String.format(
+            "%sRadius: %.1f, Height: %.1f",
+            super.toString(),
+            this.radius,
+            this.height
+        );
     }
 }
